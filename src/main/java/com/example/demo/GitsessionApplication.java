4e405dd79f929ec9a56f111247b6819da0d35da2
Sonu <sonuprakashbta@gmail.com>
2026-01-21 20:04:34 +0530

    this is my first commit

---
+++ src/main/java/com/example/demo/GitsessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsessionApplication.class, args);
+	}
+
+}
